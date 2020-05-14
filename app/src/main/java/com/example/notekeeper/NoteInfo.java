a070a88a36f4b1971c3c71a0107a0af995dcc4f7
Mwai Joseph <=>
2020-05-14 13:00:43 +0300

    Added the read and create new note functionality, implemented parcelable API also able to launch activities based on intents passed

--- app/src/main/java/com/example/notekeeper/NoteInfo.java
+++ app/src/main/java/com/example/notekeeper/NoteInfo.java
@@ -5,7 +5,7 @@ import android.os.Parcelable;
 
 import com.example.notekeeper.CourseInfo;
 
-public final class NoteInfo {
+public final class NoteInfo implements Parcelable {
     private CourseInfo mCourse;
     private String mTitle;
     private String mText;
@@ -14,6 +14,12 @@ public final class NoteInfo {
         mCourse = course;
         mTitle = title;
         mText = text;
+    }
+
+    private NoteInfo(Parcel source) {
+        mCourse = source.readParcelable(CourseInfo.class.getClassLoader());
+        mTitle = source.readString();
+        mText = source.readString();
     }
 
     public CourseInfo getCourse() {
@@ -64,4 +70,28 @@ public final class NoteInfo {
         return getCompareKey();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(mCourse, 0);
+        dest.writeString(mTitle);
+        dest.writeString(mText);
+    }
+
+    public static  Parcelable.Creator<NoteInfo> CREATOR =
+            new Parcelable.Creator<NoteInfo>() {
+                @Override
+                public NoteInfo createFromParcel(Parcel source) {
+                    return new NoteInfo(source);
+                }
+
+                @Override
+                public NoteInfo[] newArray(int size) {
+                    return new NoteInfo[size];
+                }
+            };
 }
